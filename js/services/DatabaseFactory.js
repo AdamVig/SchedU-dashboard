023c5fd23afd2fe3146844b6bd27494cc97966f4
Adam Vigneaux <adamvigneaux@gmail.com>
2015-01-24 16:42:18 -0500

    Add createSession() to authenticate when connecting to server

--- js/services/DatabaseFactory.js
+++ js/services/DatabaseFactory.js
@@ -1,26 +1,27 @@
-services.factory('DatabaseFactory', ['$http', 'dbUrl', function($http, dbUrl) {
+services.factory('DatabaseFactory', ['$http', 'DatabaseConstant', function($http, DatabaseConstant) {
 
   // Enable CORS
   $http.defaults.withCredentials = true;
 
   /**
-   * Remove trailing slash
-   * @param  {string} url URL string
-   * @return {string}     URL string without trailing slash
-   */
+  * Remove trailing slash
+  * @param  {string} url URL string
+  * @return {string}     URL string without trailing slash
+  */
   function removeTrailingSlash(url) {
     if(url.substr(-1) == '/') {
       return url.substr(0, url.length - 1);
     }
     return url;
   }
+
   /**
-   * Build URL from given parts
-   * @param  {string} couchUrl     Complete URL of CouchDB server
-   * @param  {string} databaseName Name of database
-   * @param  {string} docId        Name of document
-   * @return {string}              Complete request URL
-   */
+  * Build URL from given parts
+  * @param  {string} couchUrl     Complete URL of CouchDB server
+  * @param  {string} databaseName Name of database
+  * @param  {string} docId        Name of document
+  * @return {string}              Complete request URL
+  */
   function makeRequestUrl(couchUrl, databaseName, docId) {
     var requestUrl = removeTrailingSlash(couchUrl);
     if (databaseName) {
@@ -33,84 +34,102 @@ services.factory('DatabaseFactory', ['$http', 'dbUrl', function($http, dbUrl) {
   }
 
   /**
-   * Database class
-   * @param {string} couchUrl     URL of CouchDB server
-   * @param {string} databaseName Name of database
-   */
-  function SimpleCouch(couchUrl, databaseName) {
+  * Database class
+  * @param {string} couchUrl     URL of CouchDB server
+  * @param {string} databaseName Name of database
+  */
+  function SimpleCouch(couchInfo, databaseName) {
 
     /**
-     * Get a document from the database
-     * @param  {string}  docId ID of document
-     * @return {promise}       Promise resolved by response object
-     */
-    this.get = function (docId) {
+    * Create session on CouchDB server
+    * @param  {object}  couchInfo Contains url, username, and password
+    * @return {promise}           Fulfilled by {"status": 200} from server
+    */
+    this.createSession = function (couchInfo) {
       return $http({
-        method: "GET",
-        url: makeRequestUrl(couchUrl, databaseName, docId)
+        method: "POST",
+        url: makeRequestUrl(couchInfo.url, "_session"),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        data: "name=" + couchInfo.username + "&password=" + couchInfo.password
       });
     };
 
     /**
-     * Add a document to the database with ID if specified
-     * @param  {object}  docData Data to put in place of document
-     * @param  {string}  docId   ID of document (optional)
-     * @return {promise}         Promise resolved by response object
-     */
+    * Get a document from the database
+    * @param  {string}  docId ID of document
+    * @return {promise}       Promise resolved by response object
+    */
+    this.get = function (docId) {
+      return $http({
+        method: "GET",
+        url: makeRequestUrl(couchInfo.url, databaseName, docId)
+      });
+    };
+
+    /**
+    * Add a document to the database with ID if specified
+    * @param  {object}  docData Data to put in place of document
+    * @param  {string}  docId   ID of document (optional)
+    * @return {promise}         Promise resolved by response object
+    */
     this.insert = function (docData, docId) {
       if (docId) {
         return $http({
           method: "PUT",
-          url: makeRequestUrl(couchUrl, databaseName, docId),
+          url: makeRequestUrl(couchInfo.url, databaseName, docId),
           data: docData
         });
       } else if (docData._id) {
         return $http({
           method: "PUT",
-          url: makeRequestUrl(couchUrl, databaseName, docData._id),
+          url: makeRequestUrl(couchInfo.url, databaseName, docData._id),
           data: docData
         });
       } else {
         return $http({
           method: "POST",
-          url: makeRequestUrl(couchUrl, databaseName),
+          url: makeRequestUrl(couchInfo.url, databaseName),
           data: docData
         });
       }
     };
 
     /**
-     * Delete document from database
-     * @param  {string}  docId  ID of document
-     * @param  {string}  docRev Current revision of document in database
-     * @return {promise}        Promise resolved by response object
-     */
+    * Delete document from database
+    * @param  {string}  docId  ID of document
+    * @param  {string}  docRev Current revision of document in database
+    * @return {promise}        Promise resolved by response object
+    */
     this.delete = function (docId, docRev) {
       return $http({
         method: "DELETE",
-        url: makeRequestUrl(couchUrl, databaseName, docId),
+        url: makeRequestUrl(couchInfo.url, databaseName, docId),
         params: { rev: docRev }
       });
     };
 
     /**
-     * Get all documents from database
-     * @return {promise}    Promise resolved by response object
-     */
+    * Get all documents from database
+    * @return {promise}    Promise resolved by response object
+    */
     this.getAll = function () {
       return $http({
         method: "GET",
-        url: makeRequestUrl(couchUrl, databaseName, '_all_docs'),
+        url: makeRequestUrl(couchInfo.url, databaseName, '_all_docs'),
         params: { include_docs: true }
       });
     };
+
+    this.createSession(couchInfo);
   }
 
   return {
-    'schedule':  new SimpleCouch(dbUrl, 'schedule'),
-    'user':  new SimpleCouch(dbUrl, 'user'),
-    'feedback':  new SimpleCouch(dbUrl, 'feedback'),
-    'versions':  new SimpleCouch(dbUrl, 'versions')
+    'schedule':  new SimpleCouch(DatabaseConstant, 'schedule'),
+    'user':  new SimpleCouch(DatabaseConstant, 'user'),
+    'feedback':  new SimpleCouch(DatabaseConstant, 'feedback'),
+    'versions':  new SimpleCouch(DatabaseConstant, 'versions')
   };
 
 }]);
